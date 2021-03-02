6121cb11d52b4dd477e9e09f34dfe43cbe3eb50e
wuhardy <wuhardy@163.com>
2021-03-02 13:37:09 +0800

    test

---
+++ src/com/wuhardy/Helloworld.java
@@ -0,0 +1,31 @@
+package com.wuhardy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Helloworld {
+
+
+    private static final String str = null;
+
+
+
+    public  void test(){
+
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println("helloworld");
+
+        List<String> list1 = new ArrayList<>();
+
+        String s = new String();
+        list1.add(s);
+
+
+
+
+    }
+}
